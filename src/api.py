f23f3b5b1d9fd49305efeaf909991d9e9cfd7a18
seonmin8284 <seonmin8284@naver.com>
2025-06-01 00:44:03 +0900

    평가 합치기

--- src/api.py
+++ src/api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
-from .chat import Chat
+from chat import Chat
 
 app = FastAPI(
     title="ReadMySaju API",
